ebf1eb738addd15dfa17bbe20d59a3ad03275356
Sohini1028 <sohinihait2003@gmail.com>
2025-04-01 15:33:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('I'm Sohini Hait')
+  res.send('Sohini Hait')
 })
 
 app.get('/courses', function (req, res) {
